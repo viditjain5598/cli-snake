77f2738306aa68a2ec8569cdb790ffd0a8589b2f
Vidit Jain <viditjain5598@gmail.com>
2019-12-15 18:34:49 +0530

    bug fixes

--- snake.py
+++ snake.py
@@ -2,6 +2,7 @@ import random
 import curses
 
 s = curses.initscr()
+curses.noecho()
 curses.curs_set(0)
 sh, sw = s.getmaxyx()
 w = curses.newwin(sh, sw, 0, 0)
@@ -16,6 +17,9 @@ snake = [
     [snk_y, snk_x-2]
 ]
 
+score=0
+scoreboard = [0, sw/2]
+
 food = [sh/2, sw/2]
 w.addch(int(food[0]), int(food[1]), curses.ACS_PLUS)
 
@@ -23,9 +27,17 @@ key = curses.KEY_RIGHT
 
 while True:
     next_key = w.getch()
+    if key == curses.KEY_DOWN and next_key == curses.KEY_UP:
+        next_key = key
+    elif key == curses.KEY_UP and next_key == curses.KEY_DOWN:
+        next_key = key
+    elif key == curses.KEY_LEFT and next_key == curses.KEY_RIGHT:
+        next_key = key
+    elif key == curses.KEY_RIGHT and next_key == curses.KEY_LEFT:
+        next_key = key
     key = key if next_key == -1 else next_key
   
-    if snake[0][0] in [0, sh] or snake[0][1]  in [0, sw]:
+    if snake[0][0] in [0, sh-1] or snake[0][1]  in [0, sw-1]:
         curses.endwin()
         quit()
     
@@ -43,21 +55,22 @@ while True:
         new_head[1] -= 1
     if key == curses.KEY_RIGHT:
         new_head[1] += 1
-    #key = key if next_key == -1 else next_key
 
     snake.insert(0, new_head)
 
     if snake[0] == food:
         food = None
         while food is None:
+            score += 1
             nf = [
-                random.randint(1, sh-1),
-                random.randint(1, sw-1)
+                random.randint(1, sh-2),
+                random.randint(1, sw-2)
             ]
             food = nf if nf not in snake else None
         w.addch(food[0], food[1], curses.ACS_PLUS)
     else:
         tail = snake.pop()
         w.addch(int(tail[0]), int(tail[1]), ' ')
-
+    
+#    w.addch(int(scoreboard[0]), int(scoreboard[1]), str(score)) 
     w.addch(int(snake[0][0]), int(snake[0][1]), curses.ACS_CKBOARD)
